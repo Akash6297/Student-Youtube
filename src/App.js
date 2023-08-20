9e5f6e8d781b3b8da771e79ae8b7eb08225515da
Akash Mandal <akashmandal6297@gmail.com>
2023-08-20 23:23:35 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
 
   const handleDeleteVideo = async (videoId) => {
     try {
-      await axios.delete(`/api/deleteVideo/${videoId}`);
+      await axios.delete(`https://studentyt.onrender.com/api/deleteVideo/${videoId}`);
       fetchVideos();
     } catch (error) {
       console.error('Error deleting video:', error);
@@ -42,41 +42,34 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1>YouTube Video App</h1>
-      <div className="input-container">
-        <input
-          type="text"
-          className="name-input"
-          placeholder="Your Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          className="video-url-input"
-          placeholder="Enter YouTube Video URL or Playlist URL"
-          value={videoUrl}
-          onChange={(e) => setVideoUrl(e.target.value)}
-        />
-        <button className="add-button" onClick={handleAddVideo}>
-          Add Video
-        </button>
-      </div>
+  <h1>YouTube Video App</h1>
+  <div className="input-container">
+    <div>
+      <input
+        type="text"
+        className="name-input"
+        placeholder="Your Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </div>
+    <div>
+      <input
+        type="text"
+        className="video-url-input"
+        placeholder="Enter YouTube Video URL or Playlist URL"
+        value={videoUrl}
+        onChange={(e) => setVideoUrl(e.target.value)}
+      />
+    </div>
+    <div>
+      <button className="add-button" onClick={handleAddVideo}>
+        Add Video
+      </button>
+    </div>
+    </div>
       <div className="video-list">
-        {/* {videos.map((video, index) => (
-          <div key={index} className="video-card">
-            <p className="video-name">{video.name}</p>
-            <iframe
-              title={`Video ${index + 1}`}
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${video.videoId}`}
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-            <button className='delete-button' onClick={() => handleDeleteVideo(video._id)}>Delete</button>
-          </div>
-        ))} */}
+        
         {videos.map((video, index) => (
   <div key={index} className="video-card">
     <button
