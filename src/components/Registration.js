b90188157fba6aa9305d1315f300fdb2eac8ef80
Akash Mandal <akashmandal6297@gmail.com>
2023-08-21 13:23:15 +0530

    Update

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -5,12 +5,12 @@ import axios from 'axios';
 import '../css/registration.css';
 
 function Registration() {
-  const [email, setEmail] = useState('');
+  const [email, setEmail, name, country, phone, address] = useState('');
   const [password, setPassword] = useState('');
 
   const handleRegistration = async () => {
     try {
-      await axios.post('https://studentyt.onrender.com/api/register', { email, password });
+      await axios.post('https://studentyt.onrender.com/api/register', { email, password , name, country, phone, address});
       // Registration successful, you can redirect or show a success message
     } catch (error) {
       console.error('Error registering:', error);
@@ -22,9 +22,27 @@ function Registration() {
     <div className='registration-container'>
       <h2>Register</h2>
       <input
-        type="email"
-        placeholder="Email"
-        value={email}
+        type="name"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="country"
+        placeholder="Country"
+        value={country}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="phone"
+        placeholder="Phone No"
+        value={phone}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="address"
+        placeholder="Address"
+        value={address}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
